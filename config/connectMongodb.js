d5ba05b3017ef56256b43a8e7670fff7a20df3da
sami157 <sami.eee157@gmail.com>
2025-12-24 20:19:41 +0600

    connected mongodb

---
+++ config/connectMongodb.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+dotenv.config();
+const uri = `${process.env.MONGODB_URI}`
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const db = client.db('contestHubDB');
+
+const connectMongoDB = () => {
+    async function run() {
+        try {
+            await client.connect();
+        } finally {
+        }
+    }
+    run().catch(console.dir);
+}
+
+module.exports = {
+    connectMongoDB,
+}
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const { connectMongoDB } = require('./config/connectMongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
+connectMongoDB()
+
+app.use(cors()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
